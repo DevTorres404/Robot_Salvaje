7c69f0170608c57a06714bb565b035fea9f6f56a
DevTorres404 <damian.torrescadena0563@upse.edu.ec>
2026-07-30 10:51:29 -0500

    Yamete

--- config.ts
+++ config.ts
@@ -17,15 +17,15 @@ namespace RobotSoccer {
         export const WHEEL_CIRCUMFERENCE_MM = Math.PI * WHEEL_DIAMETER_MM
         export const DRIVE_TRACK_WIDTH_MM = 120
 
-        export const DRIVE_SPEED = 50
-        export const TURN_SPEED = 60
+        export const DRIVE_SPEED = 70
+        export const TURN_SPEED = 75
         export const AUXILIARY_SPEED = 100
         export const SEARCH_SWEEP_DEGREES = 85
-        export const SEARCH_TURN_SPEED = 35
+        export const SEARCH_TURN_SPEED = 50
         export const SEARCH_ALIGN_TOLERANCE_DEGREES = 6
         export const BALL_VERIFY_MS = 300
-        export const RECOVERY_REVERSE_MS = 450
-        export const RECOVERY_TURN_MS = 300
+        export const RECOVERY_REVERSE_MS = 325
+        export const RECOVERY_TURN_MS = 240
         export const IR_BALL_SEEN_MAX = 45
         export const IR_ATTACK_DISTANCE_MAX = 15
         export const LOOP_INTERVAL_MS = 100
@@ -33,8 +33,8 @@ namespace RobotSoccer {
         // El rumbo 0 se registra al iniciar: el robot debe comenzar mirando al arco rival.
         export const GOAL_HEADING_DEGREES = 0
         export const GOAL_ALIGN_TOLERANCE_DEGREES = 8
-        export const GOAL_ALIGN_SPEED = 35
-        export const GOAL_DRIVE_MS = 900
+        export const GOAL_ALIGN_SPEED = 50
+        export const GOAL_DRIVE_MS = 650
         export const ATTACK_TIMEOUT_MS = 4500
         export const SEARCH_TIMEOUT_MS = 15000
         export const DEFEND_DURATION_MS = 2500
