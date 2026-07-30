ed52d4d1c8a588afd394a88a0a443dec7b0364cc
DevTorres404 <damian.torrescadena0563@upse.edu.ec>
2026-07-30 18:53:31 -0500

    Revert "Dios"
    This reverts commit e02cfbe93dad5c59cacf289f8e23fdba101562fd.

--- config.ts
+++ config.ts
@@ -10,8 +10,8 @@ namespace RobotSoccer {
         export const BALL_COLOR = ColorSensorColor.White
         export const OUT_OF_BOUNDS_COLOR = ColorSensorColor.White
         // Los signos adaptan la orientación mecánica de cada actuador.
-        export const LEFT_MOTOR_DIRECTION = 1
-        export const RIGHT_MOTOR_DIRECTION = 1
+        export const LEFT_MOTOR_DIRECTION = -1
+        export const RIGHT_MOTOR_DIRECTION = -1
         export const AUXILIARY_MOTOR_DIRECTION = 1
 
         export const WHEEL_DIAMETER_MM = 56
