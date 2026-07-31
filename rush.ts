7a49a25223981b0d488a4beaad8fb7b6415c840c
DevTorres404 <damian.torrescadena0563@upse.edu.ec>
2026-07-30 19:15:39 -0500

    Ensure RUSH maintains heading to goal

--- rush.ts
+++ rush.ts
@@ -1,8 +1,8 @@
 namespace RobotSoccer {
     export class RushStrategy implements Strategy {
         run(snapshot: SensorSnapshot, movement: Movement): void {
-            // Rush at full speed forward
-            movement.attackForward()
+            // Rush at full speed forward, but keep aligned with the goal
+            movement.driveTowardFieldHeading(Config.GOAL_HEADING_DEGREES)
         }
     }
 }
